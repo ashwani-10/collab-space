12d48235405bc391063dc3e650088c988e4e3f69
Ashwani <bantykmr10@gmail.com>
2026-02-07 13:19:36 +0530

    socket updated

---
+++ src/main/java/com/example/collab_space/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.example.collab_space.controller;
+
+import com.example.collab_space.requestDto.MessageDto;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class MessageController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/message")
+    public MessageDto send(MessageDto messageDto){
+        return messageDto;
+    }
+}
